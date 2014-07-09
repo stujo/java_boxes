47e3679cac46c2730fbacd61540d403c3739ad15
stujo <stujo@imagescript.com>
2014-07-09 13:08:30 -0700

    Executable Jar Weather App

---
+++ jsonweather/src/main/java/com/skillbox/boxes/json/weather/App.java
@@ -0,0 +1,11 @@
+package com.skillbox.boxes.json.weather;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+  public static void main(final String[] args) {
+    System.out.println("Hello World!");
+  }
+}
