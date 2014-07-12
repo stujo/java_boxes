e41c541ccc852c59495ccada95467014a9deb089
stujo <stujo@imagescript.com>
2014-07-12 15:10:27 -0700

    Adding Cliche command line with in project jar

--- checkout/src/main/java/com/skillbox/boxes/checkout/App.java
+++ checkout/src/main/java/com/skillbox/boxes/checkout/App.java
@@ -1,11 +1,27 @@
 package com.skillbox.boxes.checkout;
 
+import java.io.IOException;
+
+import asg.cliche.Command;
+import asg.cliche.Shell;
+import asg.cliche.ShellFactory;
+
 /**
- * Hello world!
+ * Checkout Time
  *
  */
 public class App {
-  public static void main(final String[] args) {
-    System.out.println("Hello Universe!");
+
+  @Command
+  public String loadInventory(final String inventoryFile) {
+    return "testing![" + inventoryFile + "]";
+  }
+
+  public static void main(final String[] args) throws IOException {
+    System.out.println("Welcome to the checkout!");
+    final Shell shell = ShellFactory.createConsoleShell("checkout", "",
+        new App());
+
+    shell.commandLoop();
   }
 }
